20adaa22585c7ed34ffb7b1f272550bad6e49f4e
Socheat Sok <socheatsok78@gmail.com>
2017-02-20 15:55:23 +0700

    update homepage

--- engine/resources/views/index.blade.php
+++ engine/resources/views/index.blade.php
@@ -79,15 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ config('app.name', 'Laravel') }}
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                  <a href="https://github.com/socheatsok78/SAGE-Engine/blob/master/readme.md">Getting Started</a>
+                    <a href="https://github.com/socheatsok78/SAGE-Engine/tree/master/docs">Documentation</a>
+                    <a href="https://github.com/socheatsok78/SAGE-Engine">GitHub</a>
                 </div>
             </div>
         </div>
