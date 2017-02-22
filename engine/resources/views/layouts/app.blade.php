bd565ee74014e08b84c697e83360473f04a270af
Socheat Sok <socheatsok78@gmail.com>
2017-02-22 17:08:29 +0700

    move Menu into the dropdown list, and add content into users view

--- engine/resources/views/layouts/app.blade.php
+++ engine/resources/views/layouts/app.blade.php
@@ -53,13 +53,13 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
-                           @include('layouts.import.menu')
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
+                                    @include('layouts.import.menu')
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
