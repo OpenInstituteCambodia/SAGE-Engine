ada4764ec2a7db654a8ebdc2f1f32596fbed8d1d
Socheat Sok <socheatsok78@gmail.com>
2017-02-22 16:30:45 +0700

    create table roles and seed default data

---
+++ engine/database/seeds/RoleTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+          'name' => 'Standard User',
+          'slug' => 'standard',
+          'enabled' => true
+        ]);
+        DB::table('roles')->insert([
+          'name' => 'Administrator',
+          'slug' => 'admin',
+          'enabled' => true
+        ]);
+        DB::table('roles')->insert([
+          'name' => 'Developer User',
+          'slug' => 'developer',
+          'enabled' => true
+        ]);
+    }
+}
