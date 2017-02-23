5b8028c8569022d6b01a828460de70a1f4c79ec5
Socheat Sok <socheatsok78@gmail.com>
2017-02-23 10:12:52 +0700

    add setup.sh script for easy installing and running application

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo 'Clone Ionic Template -> template Folder'
+git clone https://github.com/socheatsok78/SAGE-Template.git template && \
+cd template && \
+npm install && \
+ionic state restore
+
+echo 'Prepare Laravel App for SAGE-Engine'
+cd .. && \
+cd engine && \
+composer install && \
+composer update && \
+cp .env.example .env && \
+php artisan key:generate && \
+php artisan serve
