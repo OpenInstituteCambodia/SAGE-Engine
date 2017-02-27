d982c82e60e4e5f5472374646c9df1001b999f5a
Socheat Sok <socheatsok78@gmail.com>
2017-02-27 14:13:35 +0700

    update developer dashbaord

--- engine/resources/views/developer/index.blade.php
+++ engine/resources/views/developer/index.blade.php
@@ -3,7 +3,10 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
+          <h2>Developer Console</h2><br>
+        </div>
+        <div class="col-md-4">
             <div class="panel panel-default">
                 <div class="panel-heading">XML Validator</div>
 
@@ -12,7 +15,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8">
             <div class="panel panel-default">
                 <div class="panel-heading">Template Version</div>
 
