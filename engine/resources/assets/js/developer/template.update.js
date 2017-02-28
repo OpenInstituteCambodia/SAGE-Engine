1d724d65c0c2b988247da779ee2cb847fce573af
Socheat Sok <socheatsok78@gmail.com>
2017-02-28 13:55:58 +0700

    github template checking JS

--- engine/resources/assets/js/developer/template.update.js
+++ engine/resources/assets/js/developer/template.update.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
               }
             }
             $('a[templateID="'+info['active']+'"]').addClass('active');
-            $('a[templateID="'+info['active']+'"] > .list-group-item-heading').append('&nbsp;<span class="badge" style="background-color: #fff; color: #000;">Active</span>');
+            $('a[templateID="'+info['active']+'"] > .list-group-item-heading').append('&nbsp;<span class="badge" style="background-color: #fff; color: #000;">In Use</span>');
         });
       }
     );
