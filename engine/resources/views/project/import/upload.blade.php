e9bbed79447d5073e1ccc93528aca0bdad719572
Socheat Sok <socheatsok78@gmail.com>
2017-03-02 09:42:33 +0700

    testing folder select for upload resource

--- engine/resources/views/project/import/upload.blade.php
+++ engine/resources/views/project/import/upload.blade.php
@@ -2,9 +2,13 @@
   <div class="form-group">
     <label for="">XML Template</label>
     <input class="form-control" type="file" name="xmlfile">
-    <input class="form-control" type="hidden" name="projectName" value="{{ $project['projectName'] }}">
+  </div>
+  <div class="form-group">
+    <label for="">Resources</label>
+    <input class="form-control" type="file" name="xmlresources" webkitdirectory mozdirectory msdirectory odirectory directory multiple>
   </div>
 
   {{ csrf_field() }}
+  <input class="form-control" type="hidden" name="projectName" value="{{ $project['projectName'] }}">
   <button class="btn btn-primary" type="submit" name="button">Upload</button>
 </form>
